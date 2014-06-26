3bffa6a37be73c8e70e676671b8b1248df8d67e3
Ahmet Celik <ahmetcelik@google.com>
2014-06-26 16:14:16 -0700

    Formatted codes

--- build_time/src/cleanup.py
+++ build_time/src/cleanup.py
@@ -28,5 +28,6 @@ def clean_invalid_glyphs_and_remove_hinting(fontfile, hinting, output):
 
 def cleanup(fontfile, hinting, output):
   """Calls cleanup pipeline Each routine must have three arguments fontfile,hinting and output
+
   """
   clean_invalid_glyphs_and_remove_hinting(fontfile, hinting, output)
