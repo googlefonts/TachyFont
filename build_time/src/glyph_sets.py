b1150d98a95aa244f6fd5e5e9e48b5e810b17575
Ahmet Celik <ahmetcelik@google.com>
2014-06-26 16:14:16 -0700

    Formatted codes

--- build_time/src/glyph_sets.py
+++ build_time/src/glyph_sets.py
@@ -14,41 +14,43 @@
   limitations under the License.
 """
 
-"""
-White space character set.
-Source: http://www.unicode.org/faq/unsup_char.html
-See 'White_Space' in http://www.unicode.org/Public/UCD/latest/ucd/PropList.txt
+"""White space character set.
+
+Source: http://www.unicode.org/faq/unsup_char.html See 'White_Space' in
+http://www.unicode.org/Public/UCD/latest/ucd/PropList.txt
     TODO(bstell) separate out the patern white space into its own list
 """
-_white_space_glyphs = [ 0x0020, 0x0085,  0x00A0, 0x1680, 0x202F, 0x205F, 0x3000,
-                      (0x0009, 0x000D),(0x2000, 0x200A),(0x2028, 0x2029) ]
+_white_space_glyphs = [0x0020, 0x0085, 0x00A0, 0x1680, 0x202F, 0x205F, 0x3000,
+                       (0x0009, 0x000D), (0x2000, 0x200A), (0x2028, 0x2029)]
 
-"""
-Default ignorable character set
-Source: http://www.unicode.org/L2/L2002/02368-default-ignorable.pdf
+"""Default ignorable character set Source: http://www.unicode.org/L2/L2002/02368-default-ignorable.pdf
+
     "Default-ignorable code points ... have no visible glyph"
     TODO(bstell) separate out the variation selectors into its own list
 """
-_default_ignorable_glyphs = [0x00AD, 0x034F, 0x061C, 0x115F, 0x1160, 0x17B4, 
-                            0x17B5, 0x3164, 0xFEFF, 0xFFA0,(0x180B, 0x180E),
-                            (0x200B, 0x200F),(0x202A, 0x202E),(0x2060, 0x206F),
-                            (0xFE00, 0xFE0F),(0x1D173, 0x1D17A)]
+_default_ignorable_glyphs = [
+    0x00AD, 0x034F, 0x061C, 0x115F, 0x1160, 0x17B4, 0x17B5, 0x3164, 0xFEFF,
+    0xFFA0, (0x180B, 0x180E), (0x200B, 0x200F), (0x202A, 0x202E),
+    (0x2060, 0x206F), (0xFE00, 0xFE0F), (0x1D173, 0x1D17A)]
 
 
-_exceptional_glyph_lists= [ _white_space_glyphs , _default_ignorable_glyphs]
+_exceptional_glyph_lists = [_white_space_glyphs, _default_ignorable_glyphs]
 
-def _expand_range_into_list(bounds,list):
-  list.extend(range(bounds[0],bounds[1]+1))
-  
+
+def _expand_range_into_list(bounds, list):
+  list.extend(range(bounds[0], bounds[1] + 1))
+
+
 def _expand_ranges(bound_list):
   expanded_list = []
   for bound in bound_list:
     if type(bound) is tuple:
-      _expand_range_into_list(bound,expanded_list)
+      _expand_range_into_list(bound, expanded_list)
     else:
       expanded_list.append(bound)
   return expanded_list
-  
+
+
 def get_whitespace_list():
   exception_set = set()
   for l in _exceptional_glyph_lists:
