c000bfb9d8334e265f3196ee67973d9e6f6505d4
Ahmet Celik <ahmetcelik@google.com>
2014-07-17 11:52:44 -0700

    In build time CharStrings index offsets are segmented

--- build_time/src/base_fonter.py
+++ build_time/src/base_fonter.py
@@ -19,6 +19,8 @@ from filler import Filler
 from fontTools.cffLib import Index
 import array
 from rle_font import RleFont
+import sys
+import struct
 
 
 
@@ -67,31 +69,57 @@ class BaseFonter(object):
     filler.fill(offset, size, '\x0e')
     filler.close()
     
+
+  def __segment_table(self, locations, off_format):
+    n = len(locations)
+    block_count = (n - 1) / BaseFonter.LOCA_BLOCK_SIZE
+    for block_no in xrange(block_count):
+      lower = block_no * BaseFonter.LOCA_BLOCK_SIZE
+      upper = (block_no + 1) * BaseFonter.LOCA_BLOCK_SIZE
+      locations[lower:upper] = array.array(off_format, [locations[upper - 1]] * BaseFonter.LOCA_BLOCK_SIZE)
+    else:
+      lower = block_count * BaseFonter.LOCA_BLOCK_SIZE
+      upper = n
+      assert upper - lower <= BaseFonter.LOCA_BLOCK_SIZE
+      locations[lower:upper] = array.array(off_format, [locations[-1]] * (upper - lower))
+
   def __fill_char_strings(self,output):
     self.font = TTFont(output)
     assert 'CFF ' in self.font
     cffTableOffset = self.font.reader.tables['CFF '].offset
     cffTable = self.font['CFF '].cff
     assert len(cffTable.fontNames) == 1
-    charStringOffset = cffTable[cffTable.fontNames[0]].rawDict['CharStrings']
-    inner_file = self.font.reader.file
-    inner_file.seek(cffTableOffset + charStringOffset)
-    rawIndexFile = Index(inner_file)
-    locations = rawIndexFile.offsets
-    off_format = '>L'
-    n = len(locations)
-    block_count = (n-1) / BaseFonter.LOCA_BLOCK_SIZE
-    for block_no in xrange(block_count):
-      lower =  block_no * BaseFonter.LOCA_BLOCK_SIZE
-      upper = (block_no+1) * BaseFonter.LOCA_BLOCK_SIZE
-      locations[lower:upper] = array.array(off_format,[locations[upper-1]] * BaseFonter.LOCA_BLOCK_SIZE)
-    else:
-      lower =  block_count * BaseFonter.LOCA_BLOCK_SIZE
-      upper = n
-      assert upper-lower <= BaseFonter.LOCA_BLOCK_SIZE
-      locations[lower:upper] =  array.array(off_format,[locations[-1]]*(upper-lower))
     
-
+    charStringOffset = cffTable[cffTable.fontNames[0]].rawDict['CharStrings']
+    
+    inner_file = self.font.reader.file
+    
+    inner_file.seek(cffTableOffset + charStringOffset)
+    count = struct.unpack('>H',inner_file.read(2))[0]
+    offSize = struct.unpack('B',inner_file.read(1))[0]
+    
+    inner_file.seek(cffTableOffset + charStringOffset )
+    raw_index_file = Index(inner_file)
+    
+    locations = raw_index_file.offsets
+    assert (count+1) == len(locations)
+    
+    self.font.close()
+    
+    off_format = 'L'
+    self.__segment_table(locations, off_format)
+    
+    new_offsets = bytearray()
+    offSize = -offSize
+    for offset in locations:
+      bin_offset = struct.pack(">l", offset)[offSize:]
+      new_offsets.extend(bin_offset)
+    assert len(new_offsets) == (count+1) * -offSize
+    
+    font_file = open(output,'r+b')
+    font_file.seek(cffTableOffset + charStringOffset + 3)
+    font_file.write(new_offsets)
+    font_file.close()
 
   def __fill_loca(self, output):  # more advanced filling needed
     self.font = TTFont(output)
@@ -107,17 +135,7 @@ class BaseFonter(object):
     locations = array.array(off_format)
     font_file.seek(loca_off);
     locations.fromstring(font_file.read(loca_len))
-    n = len(locations)
-    block_count = (n-1) / BaseFonter.LOCA_BLOCK_SIZE
-    for block_no in xrange(block_count):
-      lower =  block_no * BaseFonter.LOCA_BLOCK_SIZE
-      upper = (block_no+1) * BaseFonter.LOCA_BLOCK_SIZE
-      locations[lower:upper] = array.array(off_format,[locations[upper-1]] * BaseFonter.LOCA_BLOCK_SIZE)
-    else:
-      lower =  block_count * BaseFonter.LOCA_BLOCK_SIZE
-      upper = n
-      assert upper-lower <= BaseFonter.LOCA_BLOCK_SIZE
-      locations[lower:upper] =  array.array(off_format,[locations[-1]]*(upper-lower))
+    self.__segment_table(locations, off_format)
     font_file.seek(loca_off);
     loca_data = locations.tostring()
     assert len(loca_data)==loca_len
