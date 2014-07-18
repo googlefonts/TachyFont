c589a0ab5e509f24c50358be971c7f37d85f89cc
Ahmet Celik <ahmetcelik@google.com>
2014-07-18 16:30:58 -0700

    minor typo

--- run_time/src/gae_server/chrome/js/binary-font-editor.js
+++ run_time/src/gae_server/chrome/js/binary-font-editor.js
@@ -376,7 +376,7 @@ BinaryFontEditor.prototype.parseBaseHeader = function(font) {
         for (var i = 0; i < count; i++) {
             tag = this.readString_(4);
             tagOffset = this.getUint16_();
-            if (!BinaryFontEditor.TAG.hasOwnProperty(tag)) {//unknown tag
+            if (!BinaryFontEditor.TAGS.hasOwnProperty(tag)) {//unknown tag
                 throw 'Unknown Base Font Header TAG';
             }
             saveOffset = this.tell();
