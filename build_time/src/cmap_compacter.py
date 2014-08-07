fa7d62bc5b85c75dd598d199ead4042aaebcfc7e
Ahmet Celik <ahmetcelik@google.com>
2014-08-06 18:08:34 -0700

    number Encoders are added

--- build_time/src/cmap_compacter.py
+++ build_time/src/cmap_compacter.py
@@ -18,7 +18,35 @@ from fontTools_wrapper_funcs import _decompile_in_cmap_format_12_13,\
 from fontTools.ttLib.tables import _c_m_a_p
 import struct
 
+class NumberEncoders(object):
 
+
+  @staticmethod
+  def NibbleBin(number):
+    assert number > 0,'takes only positive numbers'
+    count = 0
+    copy_number = number if number != 0 else 1
+    while copy_number:
+      copy_number >>= 4
+      count += 1
+    return [count,bin(number)[2:].zfill(count*4)]
+        
+  @staticmethod
+  def NoNString(number):
+    result = NumberEncoders.NibbleBin(abs(number))
+    if number >= 0:
+      result[0] -= 1
+    else:
+      result[0] += 7
+    nibble_count = NumberEncoders.NibbleBin(result[0])
+    return [nibble_count,result[1]]
+      
+  @staticmethod
+  def AOE(number):
+    if number >= 15:
+      return [15,NumberEncoders.NoNString(number)]
+    else:
+      return [number,None]
 
 class _GOSGenerators(object):
   
@@ -32,7 +60,6 @@ class _GOSGenerators(object):
         table_format_12 = table
         break
     assert table_format_12,'Format 12 must exist'
-    pass
     ourData = table_format_12.cmap
     nGroups = len(ourData['startCodes'])
     gos_data = bytearray()
@@ -43,7 +70,23 @@ class _GOSGenerators(object):
     change_method(_c_m_a_p.cmap_format_12_or_13,old_12_method,'decompile')
     return gos_data
   
-GOS_Types = {5:_GOSGenerators.type5}
+  @staticmethod
+  def type3(font):
+    old_12_method = change_method(_c_m_a_p.cmap_format_12_or_13,_decompile_in_cmap_format_12_13, 'decompile')
+    cmapTable = font['cmap']
+    table_format_12 = None
+    for table in cmapTable.tables:
+      if table.format == 12:
+        table_format_12 = table
+        break
+    assert table_format_12,'Format 12 must exist'
+    
+    
+    change_method(_c_m_a_p.cmap_format_12_or_13,old_12_method,'decompile')
+    return gos_data    
+  
+GOS_Types = {5:_GOSGenerators.type5,
+             3:_GOSGenerators.type3}
 
 
 class CmapCompacter(object):
