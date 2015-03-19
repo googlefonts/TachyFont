a67ce02827e0105ea40e8f387f4fd4a80d5aa40d
Brian Stell <bstell@google.com>
2015-03-18 21:08:10 -0700

    Move the RLE decoder to a separate file.

--- run_time/src/gae_server/chrome/js/tachyfont.js
+++ run_time/src/gae_server/chrome/js/tachyfont.js
@@ -30,6 +30,7 @@ goog.require('goog.style');
 goog.require('tachyfont.BinaryFontEditor');
 goog.require('tachyfont.chainedPromises');
 goog.require('tachyfont.promise');
+goog.require('tachyfont.RLEDecoder');
 goog.require('webfonttailor');
 goog.require('webfonttailor.FontsInfo');
 
@@ -2506,127 +2507,6 @@ tachyfont.GoogleBackendService.prototype.compressedGlyphsList_ = function(
   }
   return result;
 };
-
-/**
- * tachyfont.RLEDecoder class to decode RLE'd data
- * @constructor
- */
-tachyfont.RLEDecoder = function() {};
-
-/**
- * Defined RLE operations
- * @type {Object}
- */
-tachyfont.RLEDecoder.RLE_OPS = {
-    0xC0: 'copy',
-    0xC8: 'fill'
-};
-
-/**
- * Masks to interpret byte code
- * @type {Object}
- */
-tachyfont.RLEDecoder.MASKS = {
-    SIZE: 0x03,
-    OP: 0xFC
-};
-
-/**
- * Interpret the byte code
- * @param {number} op Byte code
- * @return {Array.<number>} Array of byte cound and operation
- */
-tachyfont.RLEDecoder.byteOp = function(op) {
-  var byteCount = op & tachyfont.RLEDecoder.MASKS.SIZE;
-  var byteOperation =
-    tachyfont.RLEDecoder.RLE_OPS[op & tachyfont.RLEDecoder.MASKS.OP];
-  return [byteCount, byteOperation];
-};
-
-/**
- * Decode given rle encoded data and return decoded data
- * @param {Array.<DataView>} arr Holds the Rle encoded header and font data.
- * @return {DataView} Decoded data
- */
-tachyfont.RLEDecoder.rleDecode = function(arr) {
-  // time_start('rle');
-  var header_data = arr[0];
-  var fontdata = arr[1];
-  var readOffset = 0;
-  var writeOffset = 0;
-  var totalSize = fontdata.getUint32(readOffset);
-  if (header_data) {
-    writeOffset = header_data.byteLength;
-    totalSize += writeOffset;
-  }
-  var fill_byte;
-  var byteOperation;
-  var operationSize;
-  var operationInfo;
-  var i;
-  readOffset += 4;
-  // time_start('rle_alloc');
-  var decodedData = new DataView(new ArrayBuffer(totalSize));
-  // time_end('rle_alloc');
-  if (header_data) {
-    for (i = 0; i < header_data.byteLength; i++) {
-      decodedData.setUint8(i, header_data.getUint8(i));
-    }
-  }
-  while (writeOffset < totalSize) {
-    byteOperation = fontdata.getUint8(readOffset);
-    readOffset++;
-    operationInfo = tachyfont.RLEDecoder.byteOp(byteOperation);
-
-    if (operationInfo[0] == 0) {
-      operationSize = fontdata.getUint8(readOffset);
-      readOffset += 1;
-    } else if (operationInfo[0] == 1) {
-      operationSize = fontdata.getUint16(readOffset);
-      readOffset += 2;
-    } else if (operationInfo[0] == 2) {
-      operationSize = fontdata.getUint32(readOffset);
-      readOffset += 4;
-    }
-    if (operationInfo[1] == 'copy') {
-      // time_start('rle copy ' + operationSize);
-      // Each DataView operation is slow so minimize the number of operations.
-      // https://code.google.com/p/chromium/issues/detail?id=225811
-      var long_len = operationSize & ~3;
-      i = 0;
-      // This loop tests for "less than" but increments by 4. We know this works
-      // because the long_len was forced down to a multiple of 4.
-      for (; i < long_len; i += 4) {
-        decodedData.setUint32(writeOffset, fontdata.getUint32(readOffset));
-        readOffset += 4;
-        writeOffset += 4;
-      }
-      for (; i < operationSize; i++) {
-        decodedData.setUint8(writeOffset, fontdata.getUint8(readOffset));
-        readOffset++;
-        writeOffset++;
-      }
-      // time_end('rle copy ' + operationSize);
-    } else if (operationInfo[1] == 'fill') {
-      fill_byte = fontdata.getUint8(readOffset);
-      // time_start('rle fill ' + fill_byte + ' ' + operationSize);
-      readOffset++;
-      if (fill_byte != 0) {
-        for (i = 0; i < operationSize; i++) {
-          decodedData.setUint8(writeOffset, fill_byte);
-          writeOffset++;
-        }
-      } else {
-        writeOffset += operationSize;
-      }
-      // time_end('rle fill ' + fill_byte + ' ' + operationSize);
-    }
-
-  }
-  // time_end('rle');
-  return decodedData;
-};
-
 
 
 /**
