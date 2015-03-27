720fa65f04e0753cbb1f8a8e005e42fabdfe3a7d
Brian Stell <bstell@google.com>
2015-03-27 14:54:30 -0700

    Make the font path relative to the command.

--- build_time/scripts/subset_NotoSansJP.sh
+++ build_time/scripts/subset_NotoSansJP.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+# Get the top of the tree.
+TOPDIR="`dirname $0`/../.."
+echo TOPDIR=\"${TOPDIR}\"
+
 # The font and subset text.
-FONT='src_fonts/NotoSansJP/NotoSansJP-Thin.otf'
+FONT="${TOPDIR}/src_fonts/NotoSansJP/NotoSansJP-Thin.otf"
 TEXT='ab'
 #UNICODES='U+20-3401,U+3402-3fff,U+01F110,U+01F112,U+01F113'
 #UNICODES='U+3402'
 UNICODES=''
-
-# Get the top of the tree.
-TOPDIR="`dirname $0`/../.."
-echo TOPDIR=\"${TOPDIR}\"
 
 # Get the command.
 SUBSET_CMD="${TOPDIR}/build_time/src/make_subset.py"
