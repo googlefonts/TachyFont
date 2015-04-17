3ca4f8773b1630ece032359104dd433ef3fa6aaa
Brian Stell <bstell@google.com>
2015-04-17 10:57:15 -0700

    Add a class that maps a code point to the cmap info.

---
+++ run_time/src/gae_server/www/js/tachyfont/charcmapinfo.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @license
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+goog.provide('tachyfont.CharCmapInfo');
+
+
+
+/**
+ * The cmap information for a character.
+ *
+ * @param {number} glyphId The glyph Id.
+ * @param {number|null} format4Seg The format 4 segment.
+ * @param {number|null} format12Seg The format 12 segment.
+ * @constructor
+ */
+tachyfont.CharCmapInfo = function(glyphId, format4Seg, format12Seg) {
+
+  /** @type {number} */
+  this.glyphId = glyphId;
+
+  /** @type {number|null} */
+  this.format4Seg = format4Seg;
+
+  /** @type {number|null} */
+  this.format12Seg = format12Seg;
+};
+
+
