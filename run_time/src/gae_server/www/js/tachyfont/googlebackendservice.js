0ef03dfd8bca186967d76518d84716e8d7f672a2
Brian Stell <bstell@google.com>
2015-04-22 13:34:27 -0700

    fix surrogate support in compressedGlyphsList_

--- run_time/src/gae_server/www/js/tachyfont/googlebackendservice.js
+++ run_time/src/gae_server/www/js/tachyfont/googlebackendservice.js
@@ -160,7 +160,7 @@ GoogleBackendService.prototype.compressedGlyphsList_ = function(codes) {
   for (var i = 0; i < codes.length; i++) {
     var cp = codes[i];
     if (cp != 45) { // Dash
-      result = result + String.fromCharCode(cp);
+      result = result + String.fromCodePoint(cp);
     } else {
       // Dash is a special character in the compressed glyph list and must
       // be at the start of the string.
