de50484c8d0702dd9359d20ce19b54a8677724fb
Brian Stell <bstell@google.com>
2015-06-21 19:30:35 -0700

    Access the base file in the JAR.

--- run_time/src/gae_java/TachyFont/src/com/github/googlei18n/tachyfont/TachyFontServlet.java
+++ run_time/src/gae_java/TachyFont/src/com/github/googlei18n/tachyfont/TachyFontServlet.java
@@ -1,6 +1,7 @@
 package com.github.googlei18n.tachyfont;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -14,15 +15,16 @@ public class TachyFontServlet extends HttpServlet {
     resp.setContentType("text/plain");
     String jarFilename = "fonts/noto/sans/NotoSansJP-Thin_subset_smp.TachyFont.jar";
     JarFile jarFile = new JarFile("WEB-INF/" + jarFilename);
-    resp.getWriter().println("Jar entries in " + jarFilename + ":");
+    resp.getWriter().println("Jar file: " + jarFilename);
     Enumeration<JarEntry> enumEntries = jarFile.entries();
-    resp.getWriter().println("jar file:");
+    resp.getWriter().println("jar file entries:");
     while (enumEntries.hasMoreElements()) {
       JarEntry jarEntry = enumEntries.nextElement();
       resp.getWriter().println("  " + jarEntry.getName());
     }   
 
     JarEntry base = jarFile.getJarEntry("base");
+    InputStream baseStream = jarFile.getInputStream(base);
     JarEntry closure_data = jarFile.getJarEntry("closure_data");
     JarEntry closure_idx = jarFile.getJarEntry("closure_idx");
     JarEntry codepoints = jarFile.getJarEntry("codepoints");
