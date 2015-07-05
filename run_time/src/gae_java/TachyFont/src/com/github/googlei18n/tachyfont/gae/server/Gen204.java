e9f6f80e43f29ee4e0371c86bf11b6d9341044fc
Brian Stell <bstell@google.com>
2015-07-05 12:43:49 -0700

    Fix the servlet paths. Move Gen204 into the gae/server dir.

--- run_time/src/gae_java/TachyFont/src/com/github/googlei18n/tachyfont/gae/server/Gen204.java
+++ run_time/src/gae_java/TachyFont/src/com/github/googlei18n/tachyfont/gae/server/Gen204.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.github.googlei18n.tachyfont;
+package com.github.googlei18n.tachyfont.gae.server;
 
 import java.io.IOException;
 
