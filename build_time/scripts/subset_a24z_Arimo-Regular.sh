99c208cafa93163803c971149a36f210cf24db9c
Brian Stell <bstell@google.com>
2016-01-14 18:14:02 -0800

    Scripts to process Arimo.

---
+++ build_time/scripts/subset_a24z_Arimo-Regular.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script creates a subset of 'a' followed by 24 characters then 'z'.
+
+# Get the top of the tree.
+TOPDIR="`dirname $0`/../.."
+#echo TOPDIR=\"${TOPDIR}\"
+
+SUBSET_POSTFIX='_a24z'
+# The font and subset text.
+FONT="${TOPDIR}/src_fonts/Arimo/Arimo-Regular.ttf"
+TEXT='abcdefghijklmnopqrstuvwxyz'
+UNICODES=''
+
+# Get the command.
+SUBSET_CMD="${TOPDIR}/build_time/src/make_subset.py"
+#echo SUBSET_CMD=\"${SUBSET_CMD}\"
+
+# Point to the libraries.
+export PYTHONPATH=${PYTHONPATH}:${TOPDIR}/run_time/src/gae_server/third_party/fonttools/Lib/
+
+# Run the command.
+echo python ${SUBSET_CMD} ${FONT} --subset_postfix=${SUBSET_POSTFIX} --text=${TEXT} --unicodes=${UNICODES}
+#python ${SUBSET_CMD} ${FONT} --subset_postfix=${SUBSET_POSTFIX} --text=${TEXT} --unicodes=${UNICODES}
+
